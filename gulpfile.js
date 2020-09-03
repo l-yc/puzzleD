9dc3dca60b891707d6384112bf5591e927db6f4e
l-yc <22719973+l-yc@users.noreply.github.com>
2020-09-03 18:13:30 +0800

    add gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require("gulp-rename");
+
+gulp.task('styles', function() {
+  return gulp.src('./app/*/assets/css/sass/**/*.scss', {base: './'})
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename(function(path){
+      path.dirname = path.dirname.replace('sass', '')
+      console.log(path.dirname)
+    }))
+    .pipe(gulp.dest('.'));
+});
+
+//Watch task
+gulp.task('default', function() {
+  return gulp.watch('./app/*/assets/css/sass/**/*.scss', gulp.series('styles'));
+});
